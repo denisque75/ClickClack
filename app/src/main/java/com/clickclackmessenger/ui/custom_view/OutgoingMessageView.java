96a3a853c1a7eb2e8388e8e6495bd717f6c7b8be
denisque75@gmail.com <bizuwka3Zuwka>
2018-06-26 17:00:53 -0700

    added outgoing message view for received messages

---
+++ app/src/main/java/com/clickclackmessenger/ui/custom_view/OutgoingMessageView.java
@@ -0,0 +1,83 @@
+package com.clickclackmessenger.ui.custom_view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.clickclackmessenger.R;
+
+import java.util.Date;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class OutgoingMessageView extends LinearLayout {
+    private TextView timeTextView;
+    private TextView nameTextView;
+    private TextView messageTextView;
+    private CircleImageView imageView;
+
+    public OutgoingMessageView(Context context) {
+        this(context, null);
+    }
+
+    public OutgoingMessageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        setOrientation(HORIZONTAL);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        View rootView = inflate(context, R.layout.outgoing_message_view, this);
+
+        timeTextView = rootView.findViewById(R.id.outgoing_message__text_message_time);
+        nameTextView = rootView.findViewById(R.id.outgoing_message__text_message_name);
+        messageTextView = rootView.findViewById(R.id.outgoing_message__text_message_body);
+        imageView = imageView.findViewById(R.id.outgoing_message__image_message_profile);
+    }
+
+    public String getTime() {
+        return timeTextView.getText().toString();
+    }
+
+    public void setTime(String date) {
+        timeTextView.setText(date);
+    }
+
+    public String getName() {
+        return nameTextView.getText().toString();
+    }
+
+    public void setName(String name) {
+        nameTextView.setText(name);
+    }
+
+    public String getMessage() {
+        return messageTextView.getText().toString();
+    }
+
+    public void setMessage(String message) {
+        messageTextView.setText(message);
+    }
+
+    public void setTime(Date date) {
+        // TODO: 6/26/2018 setdate
+    }
+
+    public void setImage(Drawable drawable) {
+        imageView.setImageDrawable(drawable);
+    }
+
+    public void setImage(int resId) {
+        imageView.setImageResource(resId);
+    }
+
+    public void setImage(Bitmap bm) {
+        imageView.setImageBitmap(bm);
+    }
+
+}
